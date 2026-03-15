7cd6a8ec906d1185db4d61f8d93d69f73ffae9e8
Dave-23-gif <chikamaidavid98@gmail.com>
2026-03-15 09:29:53 +0300

    first commit

---
+++ src/test/java/com/dave/personalblogapi/PersonalBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dave.personalblogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
