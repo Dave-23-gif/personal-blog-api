114cbf637b0e20ece93c7421e0230f7b5a4cd636
Dave-23-gif <chikamaidavid98@gmail.com>
2026-03-15 19:23:47 +0300

    Created User entity

---
+++ src/main/java/com/dave/personalblogapi/entity/User.java
@@ -0,0 +1,41 @@
+package com.dave.personalblogapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Table(name = "users")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    private String bio;
+    private LocalDateTime createdAt;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
