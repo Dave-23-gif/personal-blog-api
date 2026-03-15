edb11617f719ff49a94d968d1c5d59db6a8c39be
Dave-23-gif <chikamaidavid98@gmail.com>
2026-03-15 09:41:12 +0300

    Connected to the database

--- src/test/java/com/dave/personalblogapi/PersonalBlogApiApplicationTests.java
+++ src/test/java/com/dave/personalblogapi/PersonalBlogApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PersonalBlogApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
